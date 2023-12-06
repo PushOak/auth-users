c8a781b9bedf9ec303613ca96c152366c4b28570
Demitry Gavrisenko <acousticvistas@gmail.com>
2023-12-06 15:05:22 +0200

    Loader component

--- client/src/components/loader/Loader.jsx
+++ client/src/components/loader/Loader.jsx
@@ -1,10 +1,23 @@
 import React from "react";
 import "./loader.scss";
+import ReactDOM from "react-dom";
+import loaderImage from "../../assets/loader.gif";
 
 export default function Loader() {
-  return (
-    <>
-      <div>Loader</div>
-    </>
+  return ReactDOM.createPortal(
+    <div className="wrapper">
+      <div className="loader">
+        <img src={loaderImage} alt="loading..." />
+      </div>
+    </div>,
+    document.getElementById("loader")
   );
 }
+
+export const Spinner = () => {
+  return (
+    <div className="--center-all">
+      <img src={loaderImage} alt="loading..." />
+    </div>
+  );
+};
