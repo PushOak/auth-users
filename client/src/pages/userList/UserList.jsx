c333d88927e4d48229214b33c0b290046e2d5d1c
Demitry Gavrisenko <acousticvistas@gmail.com>
2023-12-06 14:44:17 +0200

    User list table

--- client/src/pages/userList/UserList.jsx
+++ client/src/pages/userList/UserList.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import "./userList.scss";
+import { FaTrashAlt } from "react-icons/fa";
 import PageMenu from "../../components/pageMenu/PageMenu";
 import UserStats from "../../components/userStats/UserStats";
 import Search from "../../components/search/Search";
@@ -13,13 +14,42 @@ export default function UserList() {
           <UserStats />
 
           <div className="user-list">
-            <div className="--flex-between">
-              <span>
-                <h3>All Users</h3>
-              </span>
-              <span>
-                <Search />
-              </span>
+            <div className="table">
+              <div className="--flex-between">
+                <span>
+                  <h3>All Users</h3>
+                </span>
+                <span>
+                  <Search />
+                </span>
+              </div>
+              {/* Table */}
+              <table>
+                <thead>
+                  <tr>
+                    <th>s/n</th>
+                    <th>Name</th>
+                    <th>Email</th>
+                    <th>Role</th>
+                    <th>Change Role</th>
+                    <th>Action</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    <td>1</td>
+                    <td>Gato</td>
+                    <td>Gato@gmail.com</td>
+                    <td>Admin</td>
+                    <td>Change Role</td>
+                    <td>
+                      <span>
+                        <FaTrashAlt size={20} color="red" />
+                      </span>
+                    </td>
+                  </tr>
+                </tbody>
+              </table>
             </div>
           </div>
         </div>
