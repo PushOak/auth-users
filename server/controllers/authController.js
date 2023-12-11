d8060938575c1e746450685633ac7a8ee792528d
Demitry Gavrisenko <acousticvistas@gmail.com>
2023-12-11 10:05:23 +0200

    creating userAgent

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,6 +1,7 @@
 const asyncHandler = require("express-async-handler");
 const User = require("../models/userModel");
 const bcrypt = require("bcryptjs");
+const parser = require("ua-parser-js");
 const { generateToken } = require("../utils");
 
 const registerUser = asyncHandler(async (req, res) => {
@@ -23,11 +24,16 @@ const registerUser = asyncHandler(async (req, res) => {
         throw new Error("This email is already in use.");
     };
 
+    // Get user agent
+    const ua = parser(req.headers["user-agent"]);
+    const userAgent = [ua.ua];
+
     // Create new user
     const user = await User.create({
         name,
         email,
         password,
+        userAgent,
     });
 
     // Generate web token
@@ -59,8 +65,6 @@ const registerUser = asyncHandler(async (req, res) => {
         throw new Error("Invalid user data.");
 
     }
-
-
 });
 
 module.exports = {
