11148774db145566387253c9788f60311f5b0482
Demitry Gavrisenko <acousticvistas@gmail.com>
2023-12-11 13:06:35 +0200

    getUser functionality

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -6,7 +6,24 @@ const { generateToken } = require("../utils");
 
 // Get a user
 const getUser = asyncHandler(async (req, res) => {
-    res.send("Get a single user");
+    const user = await User.findById(req.user._id);
+
+    if (user) {
+        const { _id, name, phone, bio, photo, role, isVerified } = user;
+
+        res.status(200).json({
+            _id,
+            name,
+            phone,
+            bio,
+            photo,
+            role,
+            isVerified,
+        });
+    } else {
+        res.status(404);
+        throw new Error("User not found.");
+    }
 });
 
 module.exports = {
