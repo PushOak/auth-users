b2119ba27a4bd10dab037c4a232b0a8d2c547e3d
Demitry Gavrisenko <acousticvistas@gmail.com>
2023-12-11 09:13:22 +0200

    token expiresIn fix

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,6 +1,9 @@
 const asyncHandler = require("express-async-handler");
 const User = require("../models/userModel");
 const bcrypt = require("bcryptjs");
+const { generateToken } = require("../utils");
+
+
 
 const registerUser = asyncHandler(async (req, res) => {
     const { name, email, password } = req.body;
@@ -30,7 +33,35 @@ const registerUser = asyncHandler(async (req, res) => {
     });
 
     // Generate web token
-    
+    const token = generateToken(user._id);
+
+    // Send HTTP-only cookie
+    res.cookie("token", token, {
+        path: "/",
+        httpOnly: true,
+        expires: new Date(Date.now() + 1000 * 86400), // this is 1 day
+        sameSite: "none",
+        secure: true,
+    });
+
+    if (user) {
+        const { _id, name, phone, bio, photo, role, isVerified } = user;
+
+        res.status(201).json({
+            _id,
+            name,
+            phone,
+            bio,
+            photo,
+            role,
+            isVerified,
+            token,
+        });
+    } else {
+        throw new Error("Invalid user data.");
+
+    }
+
 
 });
 
