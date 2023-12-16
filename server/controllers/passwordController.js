c9d0763eff08e83080f8b43f614e96d8fd2df934
Demitry Gavrisenko <acousticvistas@gmail.com>
2023-12-16 16:14:28 +0200

    resetPassword functionality

--- server/controllers/passwordController.js
+++ server/controllers/passwordController.js
@@ -66,6 +66,35 @@ const forgotPassword = asyncHandler(async (req, res) => {
 
 });
 
+// Reset password
+const resetPassword = asyncHandler(async (req, res) => {
+    const { resetToken } = req.params;
+    const { password } = req.body;
+
+    const hashedToken = hashToken(resetToken);
+
+    const userToken = await Token.findOne({
+        resetToken: hashedToken,
+        expiresAt: { $gt: Date.now() },
+    });
+
+    if (!userToken) {
+        res.status(404);
+        throw new Error("Invalid or expired Token.");
+    }
+
+    // find user
+    const user = await User.findOne({ _id: userToken.userId });
+
+    // now - reset user's password
+    user.password = password;
+    await user.save();
+
+    res.status(200).json({ message: "Password reset successful! Please login." });
+
+});
+
 module.exports = {
     forgotPassword,
+    resetPassword,
 };
--- server/routes/passwordRoute.js
+++ server/routes/passwordRoute.js
@@ -2,9 +2,9 @@ const express = require("express");
 const router = express.Router();
 
 const { protect } = require("../middleware/authMiddleware");
-const { forgotPassword } = require("../controllers/passwordController");
+const { forgotPassword, resetPassword } = require("../controllers/passwordController");
 
 router.post("/forgot-password", forgotPassword);
-
+router.patch("/reset-password/:resetToken", resetPassword);
 
 module.exports = router;
