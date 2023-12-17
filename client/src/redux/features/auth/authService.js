610aeac87f76674983b9d4700078ae2165952b4b
Demitry Gavrisenko <acousticvistas@gmail.com>
2023-12-17 16:50:38 +0200

    creating authService file with axios

---
+++ client/src/redux/features/auth/authService.js
@@ -0,0 +1,4 @@
+import axios from "axios";
+
+const SERVER_URL = process.env.REACT_APP_SERVER_URL;
+const API_URL = `${SERVER_URL}/api/users/`;
--- client/src/redux/features/auth/authSlice.js
+++ client/src/redux/features/auth/authSlice.js
@@ -2,14 +2,31 @@ import { createSlice } from '@reduxjs/toolkit'
 
 const initialState = {
     isLoggedIn: false,
+    user: null,
+    users: [],
+    twoFactor: false,
+    isError: false,
+    isSuccess: false,
+    isLoading: false,
+    message: "",
 }
 
 const authSlice = createSlice({
     name: "auth",
     initialState,
-    reducers: {}
+    reducers: {
+        RESET(state) {
+            state.twoFactor = false;
+            state.isError = false;
+            state.isSuccess = false;
+            state.isLoading = false;
+            state.message = "";
+        },
+    }
 });
 
-export const { } = authSlice.actions;
+export const { RESET } = authSlice.actions;
+
+export const selectIsLoggedIn = (state) => state.auth.isLoggedIn;
 
 export default authSlice.reducer;
